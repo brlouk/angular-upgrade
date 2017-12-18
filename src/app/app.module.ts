5d84f9ebbb4a67099a84766a53c8817385163e4a
Brahim Loukil <bloukil@objectware.fr>
2017-12-18 14:19:58 +0100

    update structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components-2/app/app.component';
+
+import { HomeComponent } from './components-2/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
